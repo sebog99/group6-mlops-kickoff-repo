0e1d5f6ee8f6c90915ad7b34a824478182af7bf7
alberto1201 <alberto.ramirez@student.ie.edu>
2026-03-04 16:38:25 +0100

    lets go

--- tests/test_features.py
+++ tests/test_features.py
@@ -25,5 +25,7 @@ def test_engineer_features(clean_data):
 def test_get_preprocessor(clean_data):
     """Verifica que se devuelva el objeto ColumnTransformer."""
     df_eng = engineer_features(clean_data)
-    preprocessor = get_preprocessor(df_eng.drop(columns=["customerID"]))
-    assert isinstance(preprocessor, ColumnTransformer)
+    # Eliminamos el .drop() porque df_eng ya no tiene customerID
+    preprocessor = get_preprocessor(df_eng)
+
+    assert preprocessor is not None
